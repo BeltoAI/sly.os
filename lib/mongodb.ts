2cca18c7677e42510c776214003258895f0d8664
Emil Shirokikh <emilshirokikh@Emils-MacBook-Pro.local>
2025-09-29 15:53:31 -0700

    Fix MongoDB TypeScript error

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -6,10 +6,19 @@ if (!MONGODB_URI) {
   throw new Error('Please define MONGODB_URI in .env.local');
 }
 
-let cached = global.mongoose;
+interface CachedConnection {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+declare global {
+  var mongoose: CachedConnection | undefined;
+}
+
+let cached: CachedConnection = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
 }
 
 async function dbConnect() {
@@ -18,9 +27,7 @@ async function dbConnect() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose.connect(MONGODB_URI);
   }
 
   try {
